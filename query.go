557ea3b6725fd1ba948b758fb3d7887de4506714
Austin M Crane <austin.crane@ennovar.wichita.edu>
2019-08-03 09:25:09 -0500

    added Connect

--- query.go
+++ query.go
@@ -3,9 +3,27 @@ package dbutil
 // GetIDList - Given a query that selects ids, return an array of those ids
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/pkg/errors"
 )
+
+func Connect(dbName string, dbUser string, dbPassword string, dbHost string, dbPort int) (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, errors.Wrap(err, "error opening connecting to database")
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, errors.Wrap(err, "error pinging database")
+	}
+
+	return db, nil
+}
 
 func GetIDList(tx *sql.Tx, query string, args []interface{}) ([]int, error) {
 	rows, err := tx.Query(query, args...)
